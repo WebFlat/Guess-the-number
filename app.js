c2894ec2ce296d9c6c9cd84abd6a7c60e1edde5d
WebFlat <y.vacheslav@gmail.com>
2020-03-21 22:42:42 +0200

    add amination

--- app.js
+++ app.js
@@ -8,31 +8,36 @@ function randOut() {
     img = document.getElementById('img');
     if (isNaN(num)) {
         out.innerHTML = 'Введи число!!!';
+        out.style.animation = 'jitter 2s';
         img.setAttribute('src', './img/sad.png');
-        img.style.animation = "anim 2s linear";
+        img.style.animation = 'anim 2s linear';
         setTimeout(function removeAnim() {
-            img.style.animation = "none";
-        }, 2000);
+            img.style.animation = 'none';
+            out.style.animation = 'none';
+        }, 2500);
     } else {
         if (num == mathNum) {
-            out.innerHTML = 'Молодец,ты угадал!!!'
+            out.innerHTML = 'Молодец,ты угадал!!!';
+            out.style.animation = 'yep 2s';
             img.setAttribute('src', './img/smiling.png');
             img.style.animation = "anim 2s linear";
         } else if (num < mathNum) {
             out.innerHTML = 'Введи число больше';
+            out.style.animation = 'jitter 2s';
             img.setAttribute('src', './img/sad.png');
-            img.style.animation = "anim 2s linear";
+            img.style.animation = 'anim 2s linear';
             setTimeout(function removeAnim() {
-                img.style.animation = "none";
+                img.style.animation = 'none';
+                out.style.animation = 'none';
             }, 2000);
         } else {
             out.innerHTML = 'Введи число меньше';
+            out.style.animation = 'jitter 2s';
             img.setAttribute('src', './img/sad.png');
-            img.style.animation = "anim 2s linear";
+            img.style.animation = 'anim 2s linear';
             setTimeout(function removeAnim() {
-                img.style.animation = "none";
-                num.value === ' ';
-                console.log(num);
+                img.style.animation = 'none';
+                out.style.animation = 'none';
             }, 2000);
         }
     };
