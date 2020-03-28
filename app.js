133738d0b2db2325221d06bdea506452109ebeab
WebFlat <y.vacheslav@gmail.com>
2020-03-28 23:09:33 +0200

    add default num

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var mathNum = Math.floor((Math.random() * 10) + 1);
-console.log(mathNum);
+
 
 function randOut() {
-    var num, out, img;
+    var num, out, img, defNum;
     num = document.getElementById('usernum').value;
+    defNum = document.getElementById('usernum');
     out = document.getElementById('out');
     img = document.getElementById('img');
     if (isNaN(num) || num == '') {
@@ -15,12 +16,14 @@ function randOut() {
             img.style.animation = 'none';
             out.style.animation = 'none';
         }, 2500);
+        defNum.value = '';
     } else {
         if (num == mathNum) {
             out.innerHTML = 'Молодец,ты угадал!!!';
             out.style.animation = 'yep 2s';
             img.setAttribute('src', './img/smiling.png');
             img.style.animation = "anim 2s linear";
+            defNum.value = '';
         } else if (num < mathNum) {
             out.innerHTML = 'Введи число больше';
             out.style.animation = 'jitter 2s';
@@ -30,6 +33,7 @@ function randOut() {
                 img.style.animation = 'none';
                 out.style.animation = 'none';
             }, 2000);
+            defNum.value = '';
         } else {
             out.innerHTML = 'Введи число меньше';
             out.style.animation = 'jitter 2s';
@@ -39,6 +43,8 @@ function randOut() {
                 img.style.animation = 'none';
                 out.style.animation = 'none';
             }, 2000);
+            defNum.value = '';
         }
-    };
+    }
+
 }
